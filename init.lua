2627aecd70b1d0793fc762a6e968937214bca561
Martin Hooper <martin@mjhooper.co.uk>
2026-09-16 08:11:11 +0100

    added added colour scheme file

--- init.lua
+++ init.lua
@@ -1,10 +1,13 @@
 require("config.lazy")
-require('lualine').setup {}
+require("lualine").setup {}
+require("mason").setup{}
 vim.wo.number = true
 vim.opt.termguicolors = false
 vim.g.setleader = " "
+vim.cmd[[colorscheme cobalt]]
 require("shebang").setup({
 	auto_make_executable = true,
 	env = true,
 	file_mode = "755",
 })
+
